e32bdc455a5c6d5d2a539d4e01eb85a85ccd2303
Nahid5h <nahidhassan02022002@gmail.com>
2024-05-04 11:36:49 +0600

    adding Error page

--- src/Layouts/Error.jsx
+++ src/Layouts/Error.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            opps
+        <div className='mt-5'>
+        <h1 className='text-6xl font-semibold text-yellow-300 mb-8 '>opps!!</h1>
+   
+      <Link to='/'><button className='btn text-2xl bg-lime-400'>Go back</button></Link>
+ 
         </div>
     );
 };
